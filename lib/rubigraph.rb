ed71bb0dd1c5f8ce67ce87a9c9eb8ca81dd195a1
mootoh <mootoh@d0d07461-0603-4401-acd4-de1884942a52>
2008-05-24 15:20:52 +0000

    add setter methods.
    git-svn-id: http://svn.coderepos.org/share/lang/ruby/rubigraph@12294 d0d07461-0603-4401-acd4-de1884942a52

--- lib/rubigraph.rb
+++ lib/rubigraph.rb
@@ -13,11 +13,9 @@ module Rubigraph
     attr_reader :id
 
     def initialize(id = nil)
-      if id
-        @id = Rubigraph.server.call('ubigraph.new_vertex_w_id', id)
-      else
-        @id = Rubigraph.server.call('ubigraph.new_vertex')
-      end
+      @id = id ?
+        Rubigraph.server.call('ubigraph.new_vertex_w_id', id) :
+        Rubigraph.server.call('ubigraph.new_vertex')
       raise 'Rubigraph::Vertex.initialize: cannot create vertex' if @id == -1
     end
 
@@ -28,25 +26,59 @@ module Rubigraph
     end
 
     def set_attribute(att, value)
-      if -1 == Rubigraph.server.call('ubigraph.set_vertex_attribute', @id, att, value)
+      if -1 == Rubigraph.server.call('ubigraph.set_vertex_attribute', @id, att, value.to_s)
         raise "Rubigraph::Vertex##{att}=: cannot change #{att} #{@id}"
       end
     end
 
-    def color=(color)
-      set_attribute('color', color)
+    def set_attributes(attrs)
+      attrs.each do |k, v|
+        if -1 == Rubigraph.server.call('ubigraph.set_vertex_attribute', @id, k, v.to_s)
+          raise "Rubigraph::Vertex##{k}=: cannot change #{v} #{@id}"
+        end
+      end
     end
 
-    def colour=(color)
-      self.color=(color)
+    def color=(c)
+      set_attribute('color', c)
     end
 
-    def shape=(shape)
-      set_attribute('shape', shape)
+    def colour=(c)
+      self.color=(c)
+    end
+
+    def shape=(s)
+      set_attribute('shape', s)
     end
       
-    def shapedetail(detail)
-      set_attribute('shapedetail', detail)
+    def shapedetail=(d)
+      set_attribute('shapedetail', d)
+    end
+
+    def label=(l)
+      set_attribute('label', l)
+    end
+
+    def labelpos=(p)
+      set_attribute('labelpos', p)
+    end
+
+    def size=(s)
+      set_attribute('size', s)
+    end
+
+    def fontcolor=(c)
+      set_attribute('fontcolor', c)
+    end
+
+    def fontfamily=(f)
+      # TODO: should assert 'Helvetica' | 'Times Roman'
+      set_attribute('fontfamily', f)
+    end
+
+    def fontsize=(s)
+      # TODO: should assert 10, 12, 18. 24
+      set_attribute('fontsize', s)
     end
   end # Vertex
 
@@ -65,6 +97,71 @@ module Rubigraph
         raise "Rubigraph::Edge#remove: cannot remove edge #{@id}"
       end
     end
+
+    def set_attribute(att, value)
+      if -1 == Rubigraph.server.call('ubigraph.set_edge_attribute', @id, att, value.to_s)
+        raise "Rubigraph::edge##{att}=: cannot change #{att} #{@id}"
+      end
+    end
+
+    def set_attributes(attrs)
+      attrs.each do |k, v|
+        if -1 == Rubigraph.server.call('ubigraph.set_edge_attribute', @id, k, v.to_s)
+          raise "Rubigraph::edge##{k}=: cannot change #{v} #{@id}"
+        end
+      end
+    end
+
+    def color=(c)
+      set_attribute('color', c)
+    end
+
+    def colour=(c)
+      self.color=(c)
+    end
+
+    def label=(l)
+      set_attribute('label', l)
+    end
+
+    def labelpos=(p)
+      set_attribute('labelpos', p)
+    end
+
+    def fontcolor=(c)
+      set_attribute('fontcolor', c)
+    end
+
+    def fontfamily=(f)
+      # TODO: should assert 'Helvetica' | 'Times Roman'
+      set_attribute('fontfamily', f)
+    end
+
+    def fontsize=(s)
+      # TODO: should assert 10, 12, 18. 24
+      set_attribute('fontsize', s)
+    end
+
+    def strength=(s)
+      set_attribute('strength', s)
+    end
+
+    def orientationweight=(o)
+      set_attribute('orientationweight', o)
+    end
+
+    def width=(w)
+      set_attribute('width', w)
+    end
+
+    def arrow=(a)
+      set_attribute('arrow', a)
+    end
+
+    def showstrain=(s)
+      set_attribute('showstrain', s)
+    end
+
   end # Edge
 
 
